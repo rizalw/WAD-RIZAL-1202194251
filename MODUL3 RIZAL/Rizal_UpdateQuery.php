652569195f7c1e511867170733ae2061729433b6
rizalw <rizalwidyananda@gmai;.com>
2021-11-21 20:09:56 +0700

    akhirnya beres

--- MODUL3 RIZAL/Rizal_UpdateQuery.php
+++ MODUL3 RIZAL/Rizal_UpdateQuery.php
@@ -9,12 +9,12 @@
         $bahasa = $_POST['bahasa'];
         $tag = implode(", ", $_POST['tag']);
         $query = "UPDATE buku_table SET 
-            judul_buku = $judul,
-            penulis_buku = $nama,
+            judul_buku = '$judul',
+            penulis_buku = '$nama',
             tahun_terbit = $tahun,
-            deskripsi = $deskripsi,
-            tag = $tag,
-            bahasa = $bahasa
+            deskripsi = '$deskripsi',
+            tag = '$tag',
+            bahasa = '$bahasa'
             WHERE id_buku = $id_buku";
         mysqli_query($koneksi, $query);
         header("Location: Rizal_Home.php");
