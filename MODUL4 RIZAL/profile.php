2040da4cd63aa6529074714cb4a4ffe1823bbfc7
rizalw <rizalwidyananda@gmai;.com>
2021-11-28 20:11:12 +0700

    revisi dikit

--- MODUL4 RIZAL/profile.php
+++ MODUL4 RIZAL/profile.php
@@ -13,49 +13,6 @@ session_start();
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: <?php echo (isset($_COOKIE['warna'])? substr($_COOKIE['warna'], 3, 7): '#88c8f7')?>;">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">EAD Travel</a>
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
-                        <!-- Ketika belum login -->
-                        <?php if (!isset($_SESSION["login"])) : ?>
-                            <li class="nav-item">
-                                <a class="nav-link" aria-current="page" href="registrasi.php">Registrasi</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="login.php">Login</a>
-                            </li>
-                            <!-- Ketika udah login -->
-                        <?php elseif (isset($_SESSION["login"])) : ?>
-                            <li class="nav-item">
-                                <a class="nav-link" aria-current="page" href="registrasi.php">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                                    </svg>
-                                </a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <?php
-                                    echo $_SESSION["nama"]
-                                    ?>
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="profile.php">Profile</a></li>
-                                    <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-                                </ul>
-                            </li>
-                        <?php endif; ?>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
     <?php
     if (isset($_SESSION["gagal"])) {
         echo "<div class='alert alert-danger d-flex justify-content-between' role='alert'>
@@ -78,6 +35,7 @@ session_start();
       </div>";
         unset($_SESSION['berhasil']);
     }
+    require("navbar_login.php");
     ?>
     <main class="container mt-5">
         <div class="card shadow w-75 mx-auto">
